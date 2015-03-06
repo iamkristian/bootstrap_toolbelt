a3048ed4412d1be413554117bb332fd475f10a75
Kristian Rasmussen <kr@iamkristian.com>
2015-03-06 06:54:43 +0100

    introducing link_options and list_options

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,9 +1,18 @@
 module NavigationHelper
   def nav_link(text, link_path, options = {}, &block)
-    active_class_name = current_page?(link_path) ? options.fetch(:active_class_name, 'active') : ''
+    link_options = options.fetch(:link_options, {})
+    link_options[:class] = link_classes(link_path, link_options)
+    list_options = options.fetch(:list_options, {})
+
     inner = capture(&block)
-    content_tag(:li, class: options.fetch(:class_name, '')) do
-      concat(link_to(text, link_path, class: active_class_name)).concat( block_given? ? inner : "" )
+    content_tag(:li, list_options) do
+      concat(link_to(text, link_path, link_options)).concat( block_given? ? inner : "" )
     end
+  end
+
+  private
+
+  def link_classes(link_path, options)
+    current_page?(link_path) ? options.fetch(:class, "active") : ""
   end
 end

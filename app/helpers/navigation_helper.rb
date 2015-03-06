33642dd9ab52595dd385a905bbb9b6b3f252b1f5
Kristian Rasmussen <kr@iamkristian.com>
2015-03-06 05:55:47 +0100

    fixing the class

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -2,7 +2,7 @@ module NavigationHelper
   def nav_link(text, link_path, options = {}, &block)
     active_class_name = current_page?(link_path) ? options.fetch(:active_class_name, 'active') : ''
     inner = capture(&block)
-    content_tag(:li, class: "#{options.fetch(:class_name)}") do
+    content_tag(:li, class: options.fetch(:class_name, '')) do
       concat(link_to(text, link_path, class: active_class_name)).concat( block_given? ? inner : "" )
     end
   end
